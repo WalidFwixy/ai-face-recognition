35fe78cbabe8ae09fd2337fa630f854299e082bb
WalidFwixy <ma.walidkorchi@gmail.com>
2021-08-17 15:58:27 +0100

    🚩 Importing dependecies ➕ implement react-webcam

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React, { useRef } from "react";
+import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+
 import "./style/main.scss";
 
 export default function App() {
-  return <></>;
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <main>
+      <Webcam ref={webcamRef} className="webcam_element" />
+      <canvas ref={canvasRef} className="canvas_element" />
+    </main>
+  );
 }
